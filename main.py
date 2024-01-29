4457ce053ae0011108f0aaceeac66ae9a495deb3
Abhishek Verma <abhishekverm2018@gmail.com>
2024-01-29 13:17:13 +0530

    Added documentation in main.py
    added comments telling what each line of code is doing

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# Run this piece of code after train the model using train.py file
+
+# importing libraries
 from ultralytics import YOLO
 import cv2
 
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 500)
+cap = cv2.VideoCapture(0)            # camera object
+cap.set(3, 640)                      # setting width
+cap.set(4, 500)                      # setting height
 
-pt1 = (40, 40)
-pt2 = (600, 460)
-green = (0, 255, 0)
-red = (0, 0, 255)
+pt1 = (40, 40)                        # top-left point for drawing bounding box
+pt2 = (600, 460)                        # bottom-right point for drawing bounding box
+green = (0, 255, 0)                      # BGR value for Green colour
+red = (0, 0, 255)                        # BGR value for Red colour
 
 
 # loading the model
@@ -20,9 +23,9 @@ while True:
     results = model(frame)  # predict on an image
     probs = results[0].probs.top1
     if probs == 1:
-        cv2.rectangle(frame, pt1, pt2, color=red, thickness=10)
+        cv2.rectangle(frame, pt1, pt2, color=red, thickness=10)    # drawing Red box
     elif probs == 0:
-        cv2.rectangle(frame, pt1, pt2, color=green, thickness=10)
+        cv2.rectangle(frame, pt1, pt2, color=green, thickness=10)    # drawing Green box
     else:
         pass
     print(probs)
